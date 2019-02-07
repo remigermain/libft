fc2f82b2ec320003012daafc964ad0f9687930bd
rgermain <germain.remi@protonmail.com>
2019-02-07 11:00:26 +0100

    make files

--- srcs/divers/ft_freetabi.c
+++ srcs/divers/ft_freetabi.c
@@ -1,27 +1,26 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   libft.h                                          .::    .:/ .      .::   */
+/*   ft_freetabi.c                                    .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: rgermain <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/10/07 18:10:38 by rgermain     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/01 10:58:16 by rgermain    ###    #+. /#+    ###.fr     */
+/*   Created: 2019/02/01 10:42:01 by rgermain     #+#   ##    ##    #+#       */
+/*   Updated: 2019/02/01 10:57:56 by rgermain    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#ifndef DIVERS_H
-# define DIVERS_H
+#include "libft.h"
 
-void			ft_sort_integer_table(int *tab, int size);
-void			ft_swap(int *a, int *b);
-int				ft_match(char *s1, char *s2);
-int				len_wchar_single(wchar_t c);
-int				len_wchar(wchar_t *str);
-size_t			len_wuchart(unsigned char *str);
-int				nlen_wchar(wchar_t *str, size_t len);
-void			convert_wchar(unsigned char **new, wchar_t wc, size_t *i);
-char			**ft_createtab(int nb, char c);
-void			ft_freetabi(int **tab, size_t len);
-#endif
+void	ft_freetabi(int **tab, size_t len)
+{
+	size_t	count;
+
+	count = -1;
+	if (tab == NULL)
+		return ;
+	while (++count < len)
+		free(tab[count]);
+	free(tab);
+}
