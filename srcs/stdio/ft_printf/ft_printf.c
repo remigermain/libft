b12ace513379942e949c051ef3ee41267cc826c3
rgermain <germain.remi@protonmail.com>
2019-02-18 12:00:21 +0100

    fix invalide read printf

--- srcs/stdio/ft_printf/ft_printf.c
+++ srcs/stdio/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rgermain <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/01/04 16:38:22 by rgermain     #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/31 18:22:57 by rgermain    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/02/18 12:00:08 by rgermain    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -38,7 +38,7 @@ static int	find_conv(t_pf *lst, char *str, int ret)
 	return (ret);
 }
 
-static void	ftprintf_base(char *str, t_pf *lst, size_t i, size_t j)
+static int	ftprintf_base(char *str, t_pf *lst, size_t i, size_t j)
 {
 	va_copy(lst->va_copy, lst->va_lst);
 	while (str[i] != '\0')
@@ -53,50 +53,55 @@ static void	ftprintf_base(char *str, t_pf *lst, size_t i, size_t j)
 	}
 	va_end(lst->va_lst);
 	va_end(lst->va_copy);
+	return (lst->count + lst->buff_count);
 }
 
 int			ft_sprintf(UCHAR **dest, const char *format, ...)
 {
 	t_pf	*lst;
+	int		i;
 
 	lst = lst_init();
 	va_start(lst->va_lst, format);
-	ftprintf_base((char*)format, lst, 0, 0);
+	i = ftprintf_base((char*)format, lst, 0, 0);
 	convert_buff(lst, NULL, 0);
 	*dest = lst->str;
-	return (lst->count + lst->buff_count);
+	ft_memdel((void**)&lst);
+	return (i);
 }
 
 int			ft_dprintf(int fd, const char *format, ...)
 {
 	t_pf	*lst;
+	int		i;
 
 	lst = lst_init();
 	va_start(lst->va_lst, format);
-	ftprintf_base((char*)format, lst, 0, 0);
+	i = ftprintf_base((char*)format, lst, 0, 0);
 	if (lst->count != 0)
 		write(fd, lst->str, lst->count);
 	if (lst->buff_count != 0)
 		write(fd, lst->buff, lst->buff_count);
 	if (lst->str != NULL)
-		free(lst->str);
-	free(lst);
-	return (lst->count + lst->buff_count);
+		ft_memdel((void**)&(lst->str));
+	ft_memdel((void**)&lst);
+	return (i);
 }
 
 int			ft_printf(const char *format, ...)
 {
 	t_pf	*lst;
+	int		i;
 
 	lst = lst_init();
 	va_start(lst->va_lst, format);
-	ftprintf_base((char*)format, lst, 0, 0);
+	i = ftprintf_base((char*)format, lst, 0, 0);
 	if (lst->count != 0)
 		write(1, lst->str, lst->count);
 	if (lst->buff_count != 0)
 		write(1, lst->buff, lst->buff_count);
 	if (lst->str != NULL)
-		free(lst->str);
-	free(lst);
-	return (lst->count + lst->buff_count);
+		ft_memdel((void**)&(lst->str));
+	ft_memdel((void**)&lst);
+	return (i);
 }
