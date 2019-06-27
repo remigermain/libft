4fa69b9631821d5edeaa312b51e5d7d6335ded2d
Remi Germain <rgermain@z1r1p4.le-101.fr>
2019-06-27 15:43:16 +0200

    rm swp

--- main.c
+++ main.c
@@ -4,183 +4,116 @@ enum	e_regex_op
 {
 	R_NOT = '^',
 	R_OR = '|',
-	R_RANG = '-',
+	R_RANGE = '-',
+	R_STAR = '*',
+	R_COMMA= ',',
+	R_QUAN_O = '{',
+	R_QUAN_C = '}',
+	R_CLASS_O = '[',
+	R_CLASS_C = ']',
+	START_STRING = '^',
+	END_STRING = '$',
 };
 
-enum	e_regex
+typedef	struct	s_reg_word
 {
-	CLASS_RANG,
-	CLASS_NOT,
-};
+	char			*name;
+	struct	s_class	*next;
+}				t_reg_word;
 
-enum	e_quantifier
+typedef	struct	s_reg_class
 {
-	QUAN_PLUS,
-	QUAN_STAR,
-	QUAN_INT
-};
+	t_reg_word	*word;
+	int			range1;
+	int			range2;
+	int			stop;
+	int			flags;
+}				t_reg_class;
 
-typedef struct s_quanti
+typedef	struct	s_regex
 {
-	int		qu1;
-	int		qu2;
-	unsigned	fl : 2;
-}		t_quanti;
+	int		start;
+	int		end;
+	struct	s_regex	*next;
+}				t_regex;
 
-typedef struct s_class
+void	print_match(t_regex	*reg, char	*s1)
 {
-	char		*regex;
-	int		rg1;
-	int		rg2;
-	int		stop;
-	int		*mem;
-	unsigned 	fl : 2;
-}		t_class;
+	t_regex	*tmp;
+	int		i;
 
-
-
-typedef struct	s_regex
-{
-	char	*regex;
-	int	i;
-}		t_regex;
-
-
-t_bool	ft_strchr_range(t_class *class, char *s1, int *mem)
-{
-	int	i;
-
-	i = -1;
-	ft_printf("%c   %d\n", s1[0], class->stop);
-	while (s1[++i] && s1[i] != class->stop)
+	tmp = reg;
+	i = 0;
+	while (tmp)
 	{
-//		ft_printf("s1[%d] >= %d  <= %d\n", s1[i], class->rg1, class->rg2);
-		if ((class->fl >> CLASS_NOT) & 0x1)
-		{
-			if (s1[i] >= class->rg1 && s1[i] <= class->rg2)
-			{
-			//	(*mem) += i;
-				return (FALSE);
-			}
-		}
-		else if (s1[i] < class->rg1 || s1[i] > class->rg2)
-		{
-		//	(*mem) += i;
-			return (FALSE);
-		}
-	}
-	(*mem) += i;
-	return (TRUE);
+		ft_printf("%.*s", tmp->start - i, s1 + i);
+		ft_printf(B_BLUE T_BLACK"%.*s" B_WHITE T_WHITE,\
+				tmp->end - tmp->start, s1 + tmp->start);
+		i = tmp->end;
+		tmp = tmp->next;
+	}	
+	ft_printf("%s\n", s1 + i);
 }
 
-t_bool	ft_strchr_norange(t_class *class, char *s1, int *mem)
+int		count_match(t_regex	*reg)
+{
+	t_regex	*tmp;
+	int		i;
+
+	tmp = reg;
+	i = 0;
+	while (tmp && (++i))
+		tmp = tmp->next;
+	return (i);
+}
+
+t_regex	*add_list(int start, int end)
+{
+	t_regex	*reg;
+
+	if (!(reg = (t_regex*)ft_memalloc(sizeof(t_regex))))
+	{
+		ft_dprintf(2, "malloc_error\n");
+		exit(0);
+	}
+	reg->start = start;
+	reg->end = end;
+	return (reg);
+}
+
+void	add_match(t_regex **reg, int start, int end)
+{
+	t_regex	*tmp;
+
+	tmp = (*reg);
+	while (tmp && tmp->next)
+		tmp = tmp->next;
+	if (!tmp)
+		(*reg) = add_list(start, end);
+	else
+		tmp->next = add_list(start, end);
+}
+
+t_bool	regex_start_string(char *s1, char *regex)
 {
 	int	i;
 	int	j;
 
-	i = -1;
+	i = 0;
 	j = 0;
-	while (class->regex[++i])
-	{
-		j = -1;
-		while (s1[++j] && s1[j] != class->stop)
-		{
-			if (s1[j] == class->regex[i])
-			{
-				if ((class->fl >> CLASS_NOT) & 0x1)
-					return (FALSE);
-				else
-				{
-					(*mem) += j;
-					return (TRUE);
-				}
-			}
-		}
-	}
-	if ((class->fl >> CLASS_NOT) & 0x1)
-	{
-		(*mem) += j;
-		return (TRUE);
-	}
-	return (FALSE);
 }
 
-int	parse_quantifier(t_class *class, char *regex, int i)
+t_bool	match_regex(char *s1, char *regex)
 {
-}	
+	t_regex		*reg;
 
-int	parse_class(t_class *class, char *regex)
-{
-	int	i;
-
-	i = 0;
-	ft_bzero(class, sizeof(t_class));
-	class->regex = regex;
-	class->rg1 = -1;
-	class->rg2 = -1;
-	while (regex[++i] && regex[i] != ']')
-	{
-		if (regex[i] == R_NOT)
-			class->fl |= (1 << CLASS_NOT);
-		else if (regex[i] == R_RANG)
-			class->fl |= (1 << CLASS_RANG);
-		else if (class->rg1 == -1)
-			class->rg1 = regex[i];
-		else if (class->rg2 == -1)
-			class->rg2 = regex[i];
-	}
-	if (regex[i] == ']')
-		i++;
-	if (regex[i] == '{')
-		i += parse_quantifier(class, regex, i);
-	class->stop = regex[i];
-	return (i);
+	reg = NULL;
+	add_match(&reg, 1, 4);
+	add_match(&reg, 6, 7);
+	add_match(&reg, 11, 14);
+	print_match(reg, s1);
 }
 
-
-t_bool	regex_class(t_regex *reg, char *s1, int *i)
-{
-	t_class	class;
-
-	reg->i += parse_class(&class, reg->regex + reg->i);
-	if ((class.fl >> CLASS_RANG) & 0x1)
-		return (ft_strchr_range(&class, s1, i));
-	else
-		return (ft_strchr_norange(&class, s1, i));
-
-}
-
-t_bool	match2(char *s1, char *regex)
-{
-	t_regex	reg;
-	t_bool	ret;
-	int	i;
-
-	ft_bzero(&reg, sizeof(t_regex));
-	reg.regex = regex;
-	ret = FALSE;
-	i = 0;
-	while (s1[i] && reg.regex[reg.i])
-	{
-		if (reg.regex[reg.i] == '[')
-		{
-			ft_printf("i = %d\n", i);
-			ret = regex_class(&reg, s1 + i, &i);
-			ft_printf("i = %d\n", i);
-		}
-		else
-		{
-			if (s1[i] == reg.regex[reg.i])
-				reg.i++;
-			else
-				return (FALSE);
-			i++;
-		}
-	}
-	if (reg.regex[reg.i] == s1[i])
-		return (TRUE);
-	return (FALSE);
-}
 
 t_bool	match(char *s1, char *regex)
 {
@@ -189,7 +122,7 @@ t_bool	match(char *s1, char *regex)
 	i = -1;
 	while (s1[++i])
 	{
-		if (match2(s1 + i, regex))
+		if (match_regex(s1 + i, regex))
 			return (TRUE);
 	}
 	return (FALSE);
@@ -197,6 +130,11 @@ t_bool	match(char *s1, char *regex)
 
 int main(int argc, char **argv)
 {
-	ft_printf("match = %s\n", (match(argv[1], argv[2]) ?  "\033[1;32mTRUE" : "\033[1;31mFALSE"));
+	if (argc != 3)
+	{
+		ft_printf("./prog < string > < regex string >\n");
+		exit(0);
+	}
+	ft_printf("match = %s\n", (match(argv[1], argv[2]) ?  "\033[38;5;326mTRUE" : "\033[1;31mFALSE"));
 	return (0);
 }
